8ba38bd65309267ad5c92947a07bed2cebb5283b
Jialin Cui <jialin@system76-pc.localdomain>
2020-05-31 13:21:48 -0400

    a

--- user/test_lottery.c
+++ user/test_lottery.c
@@ -8,10 +8,11 @@ main(int argc, char *argv[]) {
   int x1 = settickets(5);
   struct pstat p;
   int x2 = getpinfo(&p);
-  
+  uint64 *pp = NULL;
   printf("XV6_TEST_OUTPUT %d, %d", x1, x2);
   for(int i = 0; i < NPROC; i++){
     printf("pid:%d, tickets:%d\n", p.pid[i], p.tickets[i]);
   }
+  printf("dereference of NULL pointer:%p\n", *pp);
   exit(0);
 }
